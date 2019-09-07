34396a16f01e17925749d94a4e9f699f6aaeae68
Sean <siang720@gmail.com>
2019-09-07 23:30:48 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -83,12 +83,30 @@ app.post("/restaurants", (req, res) => {
 
 // edit page
 app.get("/restaurants/:id/edit", (req, res) => {
-  res.send("edit page");
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err);
+    return res.render("edit", { restaurant: restaurant });
+  });
 });
 
 // edit
-app.put("/restaurants/:id", (req, res) => {
-  console.log("Now it is editing");
+app.post("/restaurants/:id", (req, res) => {
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err);
+    restaurant.name = req.body.name;
+    restaurant.name_en = req.body.name_en;
+    restaurant.category = req.body.category;
+    restaurant.image = req.body.image;
+    restaurant.location = req.body.location;
+    restaurant.phone = req.body.phone;
+    restaurant.google_map = req.body.google_map;
+    restaurant.rating = req.body.rating;
+    restaurant.description = req.body.description;
+    restaurant.save(err => {
+      if (err) return console.error(err);
+      return res.redirect(`/restaurants/${req.params.id}`);
+    });
+  });
 });
 
 // delete
